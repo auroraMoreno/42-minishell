ae173321c0aa9a569fb267c7fdf1e2d0ee278836
Aurora Moreno <aurora.moreno.lopez13@gmail.com>
2025-05-02 14:15:44 +0200

    comienzo de prompt-user

--- main.c
+++ main.c
@@ -6,13 +6,37 @@
 /*   By: aumoreno < aumoreno@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 18:02:39 by aumoreno          #+#    #+#             */
-/*   Updated: 2025/05/02 13:12:50 by aumoreno         ###   ########.fr       */
+/*   Updated: 2025/05/02 14:14:59 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-    printf("Prueba makefile\n");
+    (void)argc;
+    (void)argv;
+
+    char cwd[PATH_MAX]; // poner en un struct (directory_info ?)
+    char *user; // struct directory info ? 
+    user = getenv("USER");
+    if(getcwd(cwd, sizeof(cwd)) != NULL && user != NULL)
+    { 
+        // esto concat y pasarlo a readline y hacer el bucle 
+        // faltaria tmb add lo de CTRL + D finaliza y ver lo de CTRL + C 
+        // tmb gestionar leaks y alojar mem 
+        printf("%s\n", user);
+        printf("%s\n", cwd);
+    }
+    else
+        printf("didnt work");
+
+    /*
+    while(*env)
+    {
+        printf("%s\n", *env);
+        env++;
+    }
+    */
+    return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: aumoreno < aumoreno@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 12:54:37 by aumoreno          #+#    #+#             */
-/*   Updated: 2025/05/02 13:11:46 by aumoreno         ###   ########.fr       */
+/*   Updated: 2025/05/02 13:29:54 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #define MINISHELL_H
 
 #include "./libft/libft.h"
-
+#include <linux/limits.h>
 
 #endif
 
