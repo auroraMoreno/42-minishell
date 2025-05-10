152a8d600fba494544c43c4d50fc0858ed8d4eca
Aurora Moreno <aurora.moreno.lopez13@gmail.com>
2025-05-10 12:04:00 +0200

    comienzo signal ctrl c

--- main.c
+++ main.c
@@ -6,11 +6,18 @@
 /*   By: aumoreno < aumoreno@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 18:02:39 by aumoreno          #+#    #+#             */
-/*   Updated: 2025/05/03 13:11:37 by aumoreno         ###   ########.fr       */
+/*   Updated: 2025/05/05 18:17:26 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void handle_sigint(int sig)
+{
+    // poner un new line 
+    
+
+}
 
 int main(int argc, char **argv)
 {
@@ -22,9 +29,13 @@ int main(int argc, char **argv)
     char *prompt;
     char *prompt_formatted;
     char *cmd;
+    
     user = ft_strjoin(getenv("USER"), ":~");
+    
     if (getcwd(cwd, sizeof(cwd)) != NULL && user != NULL)
     {
+        signal(SIGQUIT, SIG_IGN);
+        signal(SIGINT, handle_sigint);
         // TO_DO: alojar mem ?
         while (1)
         {
@@ -32,7 +43,7 @@ int main(int argc, char **argv)
             // prompt = ft_strjoin(prompt, "$ "); TO-DO: MIRAR ESTO DEL $
             prompt_formatted = ft_strjoin(prompt, " ");
             cmd = readline(prompt_formatted);
-
+            
             if (!cmd || !user || !prompt || !prompt_formatted) // por esto no va el ctrl c
                 break;
             printf("%s", cmd);
